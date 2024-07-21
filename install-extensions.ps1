a4787b94e3c9d6d332e97a51563781b863ed5295
Justin Horner <contact@justinhhorner.com>
2024-07-20 23:58:24 -0400

    Add extensions script

---
+++ install-extensions.ps1
@@ -0,0 +1,12 @@
+# Exported using:
+# code --list-extensions | % { "code --install-extension $_" }
+
+code --install-extension ms-dotnettools.csdevkit
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension ms-dotnettools.vscodeintellicode-csharp
+code --install-extension ms-vscode.cpptools
+code --install-extension mvllow.rose-pine
+code --install-extension rebornix.toggle
+code --install-extension visualstudiotoolsforunity.vstuc
+code --install-extension vscodevim.vim
